eca71357292e04949179135da1d5159d806e2353
Lee Smith <jesmith@temenos.com>
2017-01-12 16:51:48 -0600

    Attempt to mitigate timing attacks

--- app/services/authenticate_user.rb
+++ app/services/authenticate_user.rb
@@ -15,7 +15,10 @@ class AuthenticateUser
       @flash = "Welcome back, #{@user.name}!"
     else
       @authenticated = false
-      if @user.nil? || @user.enabled?
+      if @user.nil?
+        secure_compare_padding
+        @flash = 'The email or password you entered was not recognized. Please try again!'
+      elsif @user.enabled?
         @flash = 'The email or password you entered was not recognized. Please try again!'
       elsif !@user.enabled?
         @flash = 'Your account has been disabled!'
@@ -26,6 +29,16 @@ class AuthenticateUser
 
   def success?
     @authenticated
+  end
+
+  private
+
+  def secure_compare_padding
+    # user not found so pad response time to mitigate timing attacks & user enumeration
+    ActiveSupport::SecurityUtils.secure_compare(
+      ::Digest::SHA256.hexdigest(SecureRandom.hex 32),
+      ::Digest::SHA256.hexdigest(SecureRandom.hex 32)
+    )
   end
 
 end
